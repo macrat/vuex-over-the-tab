b8367ee865503aa539cac298930dbee70d5eb3c7
MacRat <m@crat.jp>
2018-03-24 00:28:08 +0900

    Disabled install life cycle of service worker

--- worker.js
+++ worker.js
@@ -1,3 +1,7 @@
+self.addEventListener('install', ev => self.skipWaiting());
+self.addEventListener('activate', ev => self.clients.claim());
+
+
 self.addEventListener('message', ev => {
 	self.clients.matchAll().then(clients => clients.forEach(client => {
 		if (client.id !== ev.source.id) {
